a13cc84ef3cf36b7755baa45ea796c36b5cdb1f6
oleg <oleg@albekov.ru>
2016-05-24 00:16:41 +0300

    Template for task.

---
+++ Problems/Template.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+public class Template
+{
+    public static void Main()
+    {
+#if DEBUG
+        var input = new StringReader("");
+#else
+        var input = Console.In;
+#endif
+        var output = Console.Out;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpojTasks
+﻿namespace SpojTasks
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            Template.Main();
         }
     }
 }
