a7b18f92a7d53f4af62853cec0dcb20887d1d503
oleg <oleg@albekov.ru>
2016-09-11 01:11:59 +0300

    Measure time.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace SpojTasks
+﻿using System;
+using System.Diagnostics;
+
+namespace SpojTasks
 {
     internal static class Program
     {
         private static void Main()
         {
+#if DEBUG
+            var stopwatch = Stopwatch.StartNew();
+#endif
+
             Template.Main();
+
+#if DEBUG
+            stopwatch.Stop();
+            Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds} ms.");
+#endif
         }
     }
 }
